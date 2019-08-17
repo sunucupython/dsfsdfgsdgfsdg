2be3147dfcdae3b5eefb476f582bd8aa9e610dfd
sunucupython <44699541+sunucupython@users.noreply.github.com>
2019-08-17 12:55:28 +0300

    Update dent.py

--- dent.py
+++ dent.py
@@ -1,7 +1,13 @@
 from flask import Flask,json,request
 import time
+import json
 import requests
 timestamp=int(time.time())
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
 Uagent="""Dalvik/2.1.0 (Linux; U; Android 7.1.1; General Mobile 4G Build/NMF26F)"""
 app = Flask(__name__)
 @app.route("/wakeup")
@@ -24,6 +30,46 @@ def buy():
         r = requests.post(linkbuy,headers=headerx,data=data)
         return "1"
     except:
+        return "Hata"
+
+def paketlist(token):
+    ham = {"msisdn":"","mode":"owned","products":["airtime","credit","data"]}
+    data=json.dumps(ham, sort_keys=True)
+    linkpaketlist="https://craterapi.com/api/package/advanced_search"
+    headerx={"User-Agent":Uagent,"Authorization":token,"Content-Type":"""application/json; charset=utf-8"""}
+    r = requests.post(linkpaketlist,headers=headerx,data=data)
+    return r.json()
+
+
+def get_oneprice(id,token):
+    listem = []
+    listem.append(id)
+    link="https://craterapi.com/api/package/offer/buy"
+    headerx={"User-Agent":Uagent,"Authorization":token,"Content-Type":"""application/json; charset=utf-8"""}
+    r = requests.post(link,headers=headerx,data=str(listem))
+    return r.json()[0]["price"]
+
+    
+@app.route("/pklist")
+def pklist():
+    try:
+        token= request.headers["token"]
+        paketler = paketlist(token)
+        balance = 0
+        donut=[]
+        for i in paketler:
+            paketid=i["id"]
+            paketmiktar=i["lotsize"]
+            paketdeger=get_oneprice(paketid,token)
+            balance+=(paketdeger*paketmiktar)
+            name = i["carrier"]["name"]+" "+str(i["plan"]["size"])
+            donut.append({"pname":name,"paketmiktar":paketmiktar,"paketfiyat":paketdeger,"deger":paketdeger*paketmiktar})
+        donut.reverse()
+        donut.append({"toplam":balance})
+        donut.reverse()
+        return json.dumps(donut, cls=SetEncoder)
+    except Exception as e:
+        print(e)
         return "Hata"
 @app.route("/")
 def index():
